02b797c05af368e9b69f3d85dd37a3845df01b03
Guttyon3 <y.yamaguchi0329@gmail.com>
2015-09-11 21:07:11 +0900

    StageSelectで設定した制限時間を各ステージで取得
    ・StageSelectで設定した制限時間を各ステージで取得
    ・現在クリアしている星に応じて、表示を変更
       - 星３クリアしてないなら星２まで表示
       - 星２クリアしてないなら星１まで表示など

--- Assets/TimerScript.cs
+++ Assets/TimerScript.cs
@@ -18,6 +18,55 @@ public class TimerScript : MonoBehaviour {
 	public Sprite NoStarImage;
 
 	public Image ClearGetStarImage;
+
+	public int NowStageNumber = 0;
+
+	void Start(){
+
+		for (int i=0; i<=30; i++) {
+			if (i >= 1 && i <= 9) {
+				// 現在のシーン（ステージ）名を取得
+				if (Application.loadedLevelName == "Stage0" + i) {
+					// 各星の制限時間を取得(string -> int)
+					ThreeStarsLimitTime = (  int.Parse((SaveDataScript.ThreeStarsTime[i][0]).ToString()) * 600 
+					                       + int.Parse((SaveDataScript.ThreeStarsTime[i][1]).ToString()) * 60 
+					                       + int.Parse((SaveDataScript.ThreeStarsTime[i][3]).ToString()) * 10 
+					                       + int.Parse((SaveDataScript.ThreeStarsTime[i][4]).ToString()) * 1) ;
+					TwoStarsLimitTime = (  int.Parse((SaveDataScript.TwoStarsTime[i][0]).ToString()) * 600 
+					                     + int.Parse((SaveDataScript.TwoStarsTime[i][1]).ToString()) * 60 
+					                     + int.Parse((SaveDataScript.TwoStarsTime[i][3]).ToString()) * 10 
+					                     + int.Parse((SaveDataScript.TwoStarsTime[i][4]).ToString()) * 1) ;
+					OneStarLimitTime = (  int.Parse((SaveDataScript.OneStarTime[i][0]).ToString()) * 600 
+					                    + int.Parse((SaveDataScript.OneStarTime[i][1]).ToString()) * 60 
+					                    + int.Parse((SaveDataScript.OneStarTime[i][3]).ToString()) * 10 
+					                    + int.Parse((SaveDataScript.OneStarTime[i][4]).ToString()) * 1) ;
+					NowStageNumber = i;
+					
+					Debug.Log( ThreeStarsLimitTime );
+					Debug.Log( TwoStarsLimitTime );
+					Debug.Log( OneStarLimitTime );
+				}
+			} else if (i >= 10 && i <= 30) {
+				// 現在のシーン（ステージ）名を取得
+				if (Application.loadedLevelName == "Stage" + i) {
+					// 各星の制限時間を取得(string -> int)
+					ThreeStarsLimitTime = (  int.Parse((SaveDataScript.ThreeStarsTime[i][0]).ToString()) * 600 
+					                       + int.Parse((SaveDataScript.ThreeStarsTime[i][1]).ToString()) * 60 
+					                       + int.Parse((SaveDataScript.ThreeStarsTime[i][3]).ToString()) * 10 
+					                       + int.Parse((SaveDataScript.ThreeStarsTime[i][4]).ToString()) * 1) ;
+					TwoStarsLimitTime = (  int.Parse((SaveDataScript.TwoStarsTime[i][0]).ToString()) * 600 
+					                     + int.Parse((SaveDataScript.TwoStarsTime[i][1]).ToString()) * 60 
+					                     + int.Parse((SaveDataScript.TwoStarsTime[i][3]).ToString()) * 10 
+					                     + int.Parse((SaveDataScript.TwoStarsTime[i][4]).ToString()) * 1) ;
+					OneStarLimitTime = (  int.Parse((SaveDataScript.OneStarTime[i][0]).ToString()) * 600 
+					                    + int.Parse((SaveDataScript.OneStarTime[i][1]).ToString()) * 60 
+					                    + int.Parse((SaveDataScript.OneStarTime[i][3]).ToString()) * 10 
+					                    + int.Parse((SaveDataScript.OneStarTime[i][4]).ToString()) * 1) ;
+					NowStageNumber = i;
+				}
+			}
+		}
+	}
 	
 	void Update (){
 
@@ -43,6 +92,55 @@ public class TimerScript : MonoBehaviour {
 		} else {
 			GetComponent<Text> ().text = ("99:59");
 		}
+
+		// 獲得星の表示
+		if (NowTime <= ThreeStarsLimitTime) {
+			ClearGetStarImage.sprite = ThreeStarsImage;
+
+			if (NowStageNumber >= 1 && NowStageNumber <= 9) {
+				if (PlayerPrefs.GetInt ("Stage0" + NowStageNumber + "TwoStarsClear") == 0) {
+					// もし2つ星をクリアしてないなら
+					ClearGetStarImage.sprite = TwoStarsImage;
+				}
+				if (PlayerPrefs.GetInt ("Stage0" + NowStageNumber + "OneStarClear") == 0) {
+					// もし1つ星をクリアしてないなら
+					ClearGetStarImage.sprite = OneStarImage;
+				}
+			} else if (NowStageNumber >= 10 && NowStageNumber <= 30) {
+				if (PlayerPrefs.GetInt ("Stage" + NowStageNumber + "TwoStarsClear") == 0) {
+					// もし2つ星をクリアしてないなら
+					ClearGetStarImage.sprite = TwoStarsImage;
+				}
+				if (PlayerPrefs.GetInt ("Stage" + NowStageNumber + "OneStarClear") == 0) {
+					// もし1つ星をクリアしてないなら
+					ClearGetStarImage.sprite = OneStarImage;
+				}
+			}
+		} else if (ThreeStarsLimitTime < NowTime && NowTime <= TwoStarsLimitTime) {
+			ClearGetStarImage.sprite = TwoStarsImage;
+
+			if (NowStageNumber >= 1 && NowStageNumber <= 9) {
+				if (PlayerPrefs.GetInt ("Stage0" + NowStageNumber + "OneStarClear") == 0) {
+					// もし1つ星をクリアしてないなら
+					ClearGetStarImage.sprite = OneStarImage;
+				}
+			} else if (NowStageNumber >= 10 && NowStageNumber <= 30) {
+				if (PlayerPrefs.GetInt ("Stage" + NowStageNumber + "OneStarClear") == 0) {
+					// もし1つ星をクリアしてないなら
+					ClearGetStarImage.sprite = OneStarImage;
+				}
+			}
+		} else if (TwoStarsLimitTime < NowTime && NowTime <= OneStarLimitTime) {
+			ClearGetStarImage.sprite = OneStarImage;
+		} else if (NowTime > OneStarLimitTime) {
+			ClearGetStarImage.sprite = NoStarImage;
+		}
+		/*
+		else {
+			ClearGetStarImage.sprite = NoStarImage;
+		}
+		*/
+		/*
 		if (NowTime <= ThreeStarsLimitTime) {
 			ClearGetStarImage.sprite = ThreeStarsImage;
 		} else if (ThreeStarsLimitTime < NowTime && NowTime <= TwoStarsLimitTime) {
@@ -52,6 +150,6 @@ public class TimerScript : MonoBehaviour {
 		} else {
 			ClearGetStarImage.sprite = NoStarImage;
 		}
-
+		*/
 	}
 }
