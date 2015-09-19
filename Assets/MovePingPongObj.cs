b0da93026161534443e72611874ea086c39eee24
Guttyon3 <y.yamaguchi0329@gmail.com>
2015-09-19 15:59:58 +0900

    Stage01/02完成

--- Assets/MovePingPongObj.cs
+++ Assets/MovePingPongObj.cs
@@ -2,23 +2,35 @@
 using System.Collections;
 
 public class MovePingPongObj : MonoBehaviour {
-
 	
-	public float moveDistance = 40.0f;
 	public float moveSpeed    =  4.0f;
 	float startDistance = 0.0f;
 
+	
+	public float moveDistance_X = 40.0f;
+	public float moveDistance_Y =  0.0f;
+	public float moveDistance_Z =  0.0f;
+	
+	private float startPosition_X;
+	private float startPosition_Y;
+	private float startPosition_Z;
+
 	void Start(){
-		startDistance = moveDistance;
+		if (moveDistance_X == 0) { moveDistance_X = 0.0000001f;}
+		if (moveDistance_Y == 0) { moveDistance_Y = 0.0000001f;}
+		if (moveDistance_Z == 0) { moveDistance_Z = 0.0000001f;}
+		
+		startPosition_X = transform.position.x;
+		startPosition_Y = transform.position.y;
+		startPosition_Z = transform.position.z;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-		if (startDistance >= 0) {
-			transform.position = new Vector3 (Mathf.PingPong (Time.time * moveSpeed, moveDistance) - (moveDistance / 2), transform.position.y, transform.position.z);
-		} else {
-			transform.position = new Vector3 (Mathf.PingPong (Time.time * moveSpeed, moveDistance) + (-moveDistance * 2), transform.position.y, transform.position.z);
-		}
+		transform.position = new Vector3 (startPosition_X + Mathf.PingPong (Time.time * moveSpeed, moveDistance_X) - (moveDistance_X / 2.0f)
+		                                , startPosition_Y + Mathf.PingPong (Time.time * moveSpeed, moveDistance_Y) - (moveDistance_Y / 2.0f)
+		                                , startPosition_Z + Mathf.PingPong (Time.time * moveSpeed, moveDistance_Z) - (moveDistance_Z / 2.0f));
+
 	}
 }
