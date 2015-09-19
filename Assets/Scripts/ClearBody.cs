09d4fc65bf355556bf11dce03da494128012af68
Guttyon3 <y.yamaguchi0329@gmail.com>
2015-09-19 11:15:39 +0900

    ClearBody更新
    ・プレイヤーを半透明にするときは”Transparent/Diffuse”に変更
    ・それ以外は”Standard”（あらかじめInspectorで”Opaque”に設定しておく)

--- Assets/Scripts/ClearBody.cs
+++ Assets/Scripts/ClearBody.cs
@@ -9,15 +9,27 @@ public class ClearBody : MonoBehaviour {
 	Color sColor;
 	Renderer pR;
 
+	public Shader shader1;
+	public Shader shader2;
+	
 	void Start(){
 		pR = GetComponent<Renderer>();
 		sColor = pR.material.color;
+
+		shader1 = Shader.Find("Transparent/Diffuse");
+		shader2 = Shader.Find("Standard");
+
 	}
 
 	void Update(){
 		if (Physics.Raycast (transform.position, -transform.forward, out hit, rayLength)) {
-			distanceToWall = hit.distance/rayLength;
-			pR.material.color = new Color (1,1,1,distanceToWall) * sColor;
+			distanceToWall = hit.distance / rayLength;
+			pR.material.color = new Color (1, 1, 1, distanceToWall) * sColor;
+			
+			pR.material.shader = shader1;
+		} else {
+
+			pR.material.shader = shader2;
 		}
 	}
 }
