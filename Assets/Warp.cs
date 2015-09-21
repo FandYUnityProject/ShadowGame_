be5783201ed75bbe665518d7f45f75215709ee82
whitewhale6 <whitewhale6@gmail.com>
2015-09-21 15:39:56 +0900

    level01

---
+++ Assets/Warp.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Warp : MonoBehaviour {
+
+	public GameObject toPoint;
+	public GameObject player;
+
+	void Start(){
+
+	}
+
+	void OnTriggerEnter(Collider coll){
+		if (coll.gameObject.tag == "Body") {
+			coll.gameObject.transform.position = toPoint.transform.position;
+		}
+	}
+}
