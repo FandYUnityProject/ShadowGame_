9f66015a6570ad7734e2b4fdbe13c2e3b9a8cdc0
Guttyon3 <y.yamaguchi0329@gmail.com>
2015-09-22 13:02:50 +0900

    Stage08 完成
    残りStage06,09

---
+++ Assets/Scripts/SpringAddForce.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpringAddForce : MonoBehaviour {
+
+	public float springPower = 5.0f;
+
+	private GameObject playerObj;
+
+	void Start(){
+		playerObj = GameObject.Find ("Player");
+	}
+
+	void OnTriggerEnter(Collider coll){
+
+		if (coll.gameObject.name == "Player") {
+			Debug.Log("Spring!");
+			playerObj.GetComponent<Rigidbody>().velocity = new Vector3(playerObj.GetComponent<Rigidbody>().velocity.x, 0 ,playerObj.GetComponent<Rigidbody>().velocity.z);
+			playerObj.GetComponent<Rigidbody> ().AddForce (transform.up * springPower);
+		}
+	}
+}
