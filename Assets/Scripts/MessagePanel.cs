b8d11cf49f778c70a64d6dac9b9ec05d59a742fd
Guttyon3 <y.yamaguchi0329@gmail.com>
2015-09-23 12:18:50 +0900

    各ステージにチュートリアルを設置

---
+++ Assets/Scripts/MessagePanel.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MessagePanel : MonoBehaviour {
+
+	private GameObject MessagePanelObj;
+
+	// Use this for initialization
+	void Start () {
+	
+		MessagePanelObj = GameObject.Find ("MessagePanel");
+		MessagePanelObj.SetActiveRecursively (false);
+	}
+
+	void OnTriggerStay(Collider coll){
+
+		if (coll.gameObject.name == "Player") {
+			MessagePanelObj.SetActiveRecursively (true);
+		}
+	}
+
+	void OnTriggerExit(Collider coll){
+		
+		if (coll.gameObject.name == "Player") {
+			MessagePanelObj.SetActiveRecursively (false);
+		}
+	}
+}
---
+++ Assets/Scripts/StaffRollPanel.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class StaffRollPanel : MonoBehaviour {
+
+	public GameObject StaffRollPanelObj;
+	
+	public Sprite StaffRollPanelImage;
+
+	// Use this for initialization
+	void Start () {
+	
+		StaffRollPanelObj.SetActiveRecursively (false);
+	}
+
+	void OnTriggerStay(Collider coll){
+
+		if (coll.gameObject.name == "Player") {
+			StaffRollPanelObj.SetActiveRecursively (true);
+
+			StaffRollPanelObj.GetComponent<Image>().sprite = StaffRollPanelImage;
+		}
+	}
+
+	void OnTriggerExit(Collider coll){
+		
+		if (coll.gameObject.name == "Player") {
+			StaffRollPanelObj.SetActiveRecursively (false);
+		}
+	}
+}
